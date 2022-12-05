607f6dd9f3383704179195c1178aa33433b961e3
Halil ibrahim çopur <118901793+ibrahimcpr@users.noreply.github.com>
2022-12-05 17:49:37 +0300

    Update coin_tracker.py

--- coin_tracker.py
+++ coin_tracker.py
@@ -14,7 +14,7 @@ value_label = Label(root, text="")
 value_label.place(x=25,y=150)
 coin_label=Label(root,text="Select Coin:",font=("Arial",12,"bold"))
 coin_label.place(x=25,y=0)
-state_label=Label(root,text="Select State:",font=("Arial",12,"bold"))
+state_label=Label(root,text="Select state",font=("Arial",12,"bold"))
 state_label.place(x=25,y=100)
 coinlist=["BTCUSDT","ETHUSDT","BNBUSDT","SOLUSDT","ADAUSDT","XRPUSDT","DOTUSDT","USDCUSDT","DOGEUSDT","SHIBUSDT","AVAXUSDT","LUNAUSDT","LTCUSDT","BUSDUSDT","UNIUSDT","LINKUSDT","ALGOUSDT","BCHUSDT","MATICUSDT","VETUSDT","XLMUSDT","AXSUSDT","MANAUSDT","ICPUSDT","TRXUSDT","FTTUSDT","USTUSDT","FILUSDT","DAIUSDT","ETCUSDT","EGLDUSDT","ATOMUSDT",
 "THETAUSDT","HBARUSDT","NEARUSDT","FTMUSDT","XTZUSDT","GRTUSDT","XMRUSDT","HNTUSDT","EOSUSDT","SANDUSDT","KLAYUSDT","FLOWUSDT","CAKEUSDT","AAVEUSDT","XECUSDT","KDAUSDT","LRCUSDT","KSMUSDT","NEOUSDT","MKRUSDT","QNTUSDT","ENJUSDT","CHZUSDT","RUNEUSDT","ONEUSDT","STXUSDT","WAVESUSDT","BTTUSDT","HOTUSDT","AMPUSDT","ZECUSDT","ARUSDT","DASHUSDT","COMPUSDT","CELOUSDT","CRVUSDT","NEXOUSDT","IOTXUSDT","TFUELUSDT","WAXPUSDT",
@@ -30,7 +30,7 @@ warn=Entry(root)
 warn_label = Label(root,text="Set limit price:",font=("Arial",12,"bold"))
 warn_label.place(x=25,y=50)
 cbox_state.current(2)
-warn.place(x=150,y=50)
+warn.place(x=175,y=50)
 def get_warn():
     x=float(warn.get())
     return x
@@ -43,11 +43,11 @@ def get_value():
         float_price=float(price)
         value_label.config(text=f"{data['symbol']} price is {data['price']}",font=("Arial",12,"bold"))
         if cbox_state.get()=="HIGHER" and get_warn()<float_price:
-            winsound.Beep(5000, 3000)
+            winsound.Beep(5000,1000)
             check=p.confirm("Warning your coin is higher than {}".format(get_warn()),buttons=["Okay"])
             cbox_state.current(2)         
         if cbox_state.get()=="LOWER" and get_warn()>float_price:
-            winsound.Beep(5000, 3000)
+            winsound.Beep(5000, 1000)
             check=p.confirm("Warning your coin is lower than {}".format(get_warn()),buttons=["Okay"])
             cbox_state.current(2)            
     except:KeyError
